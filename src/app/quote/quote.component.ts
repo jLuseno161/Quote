6c6dc732b682ee1b9614f830a1065c2212ee2827
jLuseno161 <joyluseno2@gmail.com>
2021-05-14 23:33:55 +0300

    edit form'

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quotes: Quotes[] = [
-    { quote: "“to learn and not to do is really not to learn. To know and not to do is really not to know.”", author: "Bill Gates", upvote: 20, downvote: 50 },
-    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Mora Savier", upvote: 200, downvote: 44},
-    { quote: "“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", author: "Christopher Crawford", upvote: 60, downvote: 44 },
-    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Billy Sunday", upvote: 602, downvote: 44 }
+    { quote: "“to learn and not to do is really not to learn. To know and not to do is really not to know.”", author: "Bill Gates",submittedBy:"Joy K",upvote: 20, downvote: 50 },
+    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Mora Savier", submittedBy:"Kirui Mart",upvote: 200, downvote: 44},
+    { quote: "“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", author: "Christopher Crawford",submittedBy:"Cindy Kip", upvote: 60, downvote: 44 },
+    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Billy Sunday",submittedBy:"Ann Mariee", upvote: 602, downvote: 44 }
 
   ]
   constructor() { }

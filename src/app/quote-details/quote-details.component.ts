e75a2316ee0e823329e073361f49c3eef3585bc8
jLuseno161 <joyluseno2@gmail.com>
2021-05-15 01:00:53 +0300

    //add input import for Quotes class to allow input prop binding

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
+import { Quotes } from '../quotes';
+
 
 @Component({
   selector: 'app-quote-details',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteDetailsComponent implements OnInit {
 
+  @Input() quote!:Quotes;
   constructor() { }
 
   ngOnInit(): void {

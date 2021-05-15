1235c0a045cfabab0c38b5a18af6fb8dfb2368b9
jLuseno161 <joyluseno2@gmail.com>
2021-05-15 10:58:32 +0300

    new Quotes object

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,17 @@ import { Quotes } from '../quotes';
 })
 export class QuoteComponent implements OnInit {
 
+  //quotes: Quotes[] = [
+  //   { quote: "“to learn and not to do is really not to learn. To know and not to do is really not to know.”", author: "Bill Gates",submittedBy:"Joy K",upvote: 20, downvote: 50 },
+  //   { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Mora Savier", submittedBy:"Kirui Mart",upvote: 200, downvote: 44},
+  //   { quote: "“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", author: "Christopher Crawford",submittedBy:"Cindy Kip", upvote: 60, downvote: 44 },
+  //   { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Billy Sunday",submittedBy:"Ann Mariee", upvote: 602, downvote: 44 }
+  // ]
   quotes: Quotes[] = [
-    { quote: "“to learn and not to do is really not to learn. To know and not to do is really not to know.”", author: "Bill Gates",submittedBy:"Joy K",upvote: 20, downvote: 50 },
-    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Mora Savier", submittedBy:"Kirui Mart",upvote: 200, downvote: 44},
-    { quote: "“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", author: "Christopher Crawford",submittedBy:"Cindy Kip", upvote: 60, downvote: 44 },
-    { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Billy Sunday",submittedBy:"Ann Mariee", upvote: 602, downvote: 44 }
-
+    new Quotes("“to learn and not to do is really not to learn. To know and not to do is really not to know.”", "Bill Gates", "Joy K", 20, 50),
+    new Quotes("“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", "Mora Savier", "Kirui Mart", 200, 44),
+    new Quotes("“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", "Christopher Crawford", "Cindy Kip", 60, 44),
+    new Quotes("“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", "Billy Sunday", "Ann Mariee", 602, 44)
   ]
   constructor() { }
 

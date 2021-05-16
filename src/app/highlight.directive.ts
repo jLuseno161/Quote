781867805273bd123ee8ffacf76d581053d93988
jLuseno161 <joyluseno2@gmail.com>
2021-05-16 17:54:53 +0300

    save changes

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef,} from '@angular/core';
 export class HighlightDirective {
 
   constructor(el: ElementRef) {
-    el.nativeElement.style.backgroundColor = 'blue';
+    el.nativeElement.style.backgroundColor = 'gainsboro';
   }
 
 }

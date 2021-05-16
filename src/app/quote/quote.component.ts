2557f6a5fe3f17a69b867babac8f42475fd4bda3
jLuseno161 <joyluseno2@gmail.com>
2021-05-16 16:46:03 +0300

    LICENCE

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -15,10 +15,10 @@ export class QuoteComponent implements OnInit {
   //   { quote: "“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", author: "Billy Sunday",submittedBy:"Ann Mariee", upvote: 602, downvote: 44 }
   // ]
   quotes: Quotes[] = [
-    new Quotes("“to learn and not to do is really not to learn. To know and not to do is really not to know.”", "Bill Gates", "Joy K", 0, 0, new Date(2000, 2, 14)),
-    new Quotes("“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", "Mora Savier", "Kirui Mart", 0, 0, new Date(1996, 7, 14)),
-    new Quotes("“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", "Christopher Crawford", "Cindy Kip", 0, 0, new Date(2020, 3, 14)),
-    new Quotes("“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", "Billy Sunday", "Ann Mariee", 0, 0, new Date(2015, 3, 23))
+    new Quotes("“to learn and not to do is really not to learn. To know and not to do is really not to know.”", "Bill Gates", "Joy K", 0, 0, new Date(2019, 2, 14)),
+    new Quotes("“The Bible will always be full of things you cannot understand, as long as you will not live according to those you can understand.”", "Mora Savier", "Kirui Mart", 0, 0, new Date(2020, 7, 14)),
+    new Quotes("“knowledge without application is like a book that is never read' Christopher Crawford, Hemel Hempstead.”", "Christopher Crawford", "Cindy Kip", 0, 0, new Date(2021, 2, 14)),
+    new Quotes("“We are what we pretend to be, so we must be careful about what we pretend to be.”", "Kurt Vonnegut, Mother Night", "Ann Mariee", 0, 0, new Date(2015, 3, 23))
   ];
   addNewQuote(quote: Quotes) {
     this.quotes.push(quote)
